f3968dae9b5b3f77b7634a242f6432782f169574
GEE <gm.walker2022@gmail.com>
2024-09-19 10:31:41 -0400

    <feat> Defined 'expandNav()'

---
+++ scripts/mobileNav.js
@@ -0,0 +1,9 @@
+const expandNav = () => {
+    var page_links = document.getElementById("pageLinks");
+    if (page_links.style.display === "block"){
+        page_links.style.display = "none";
+    }
+    else {
+        page_links.style.display = "block";
+    }
+};
