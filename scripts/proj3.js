29906db49065ef466384130100472a2d9dbb3e17
GEE <gm.walker2022@gmail.com>
2024-09-19 12:32:08 -0400

    <feat> Created Source Object, Resources Array, & 'addSource' function.

---
+++ scripts/proj3.js
@@ -0,0 +1,12 @@
+const Source = (name, url, description, provider) => {
+    this.name = name;
+    this.url = url;
+    this.description = description;
+    this.provider = provider;
+}
+
+const resources = [];
+
+const addSource = (name, url, desc, provider) => {
+    resources.push(new Source(name, url, desc, provider));
+}
