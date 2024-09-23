17e3af3a6941cbd474b26f2f215f4cc047e8c5ac
GEE <gm.walker2022@gmail.com>
2024-09-23 18:03:43 -0400

    <feat> Final Touches for Assignment 3

--- scripts/proj3.js
+++ scripts/proj3.js
@@ -1,5 +1,5 @@
 // A constructor for an object that stores information about a web resource.
-const Source = (name, url, description, provider) => {
+function Source (name, url, description, provider) {
     this.name = name;
     this.url = url;
     this.description = description;
@@ -15,9 +15,10 @@ function addSource (name, url, desc, provider){
     }
     else {
         resources.push(new Source(name, url, desc, provider));
+        console.log(`${name} was added to the Resources array.`)
     }
 }
-const displaySources = (section) => {
+const displaySources = (resources, section) => {
     section.innerHTML = '';
     
     // Accesses a 'div' element and displays all sources within it
@@ -28,14 +29,15 @@ const displaySources = (section) => {
         case 1:
             console.log("Yipee! You have a source to look at!");
             resources.forEach((source) => {
-                section.innerHTML += `<div class="resource"><h3>${source.name}</h3><p>${source.description}</p><p>${source.provider}</p></div>`
+                section.innerHTML += `<div class="resource"><h3><a href="${source.url}">${source.name}</a></h3><p>${source.description}</p></div>`;
             });
             break;
         case 2:
             console.log("Oooh! We've got two sources! Yay!");
             resources.forEach((source) => {
-                section.innerHTML += `<div class="resource"><h3>${source.name}</h3><p>${source.description}</p><p>${source.provider}</p></div>`
+                section.innerHTML += `<div class="resource"><h3><a href="${source.url}">${source.name}</a></h3><p>${source.description}</p></div>`;
             });
+            break;
         default:
             resources.forEach((source) => {
                 section.innerHTML += `<div class="resource"><h3>${source.name}</h3><p>${source.description}</p><p>${source.provider}</p></div>`
@@ -45,3 +47,18 @@ const displaySources = (section) => {
     }
 };
 
+addSource(
+    "How Animation Works - 3 Basic Principles", 
+    "https://www.youtube.com/watch?v=1KPfsCrmWzk", 
+    "Animation has the reputation of being a difficult art medium. In this video, let's reveal the true basics of animation to make the learning process easier.", 
+    "NobleFrugal Studio"
+);
+
+addSource(
+    "The Beauty of Bezier Curves",
+    "https://www.youtube.com/watch?v=aVwxzDHniEw",
+    "They're used for animation, text rendering, and all sorts of curved shapes! But how do they actually work? well, like, that's what the video is about, so, watch it to find out etc!!",
+    "Freya Holmer"
+);
+
+displaySources(resources, bibliography);
