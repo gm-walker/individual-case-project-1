4c62dbfd8aceac87a4d9d2fa4c31614145c313de
GEE <gm.walker2022@gmail.com>
2024-11-01 23:28:47 -0400

    <feat> Update Theme Changer to use Cookies

--- scripts/themeChange.js
+++ scripts/themeChange.js
@@ -1,24 +1,35 @@
 var themeToggleCount = 0;
 const themeChanger = document.getElementById("theme-chng").children[0];
 
-if(sessionStorage.getItem("theme")){
-    document.body.setAttribute('data-theme', sessionStorage.getItem("theme"));
-    if(sessionStorage.theme == "dark"){
-        themeChanger.checked = true;
+function readCookie(){
+    let cookies = {};
+    if(document.cookie){
+        let cookieList = document.cookie.split("; ");
+        cookieList.forEach((items)=>{
+            let cookie = items.split("=");
+            let name = cookie[0]
+            let value = decodeURIComponent(cookie[1]);
+            cookies[name] = value;
+        })
+        return cookies;
     }
+}
+if(readCookie()['theme'] == 'dark'){
+    themeChanger.checked = true;
+    document.body.setAttribute('data-theme', "dark");
 }
 
 function toggleTheme() {
     // Retrieves the current theme being utilized
-    sessionStorage.setItem("theme", document.body.getAttribute('data-theme'));
+    document.cookie = `theme=${document.body.getAttribute('data-theme')}`;
     
-    if(String(sessionStorage.theme) === 'dark'){
+    if(readCookie()['theme'] === 'dark'){
         document.body.setAttribute('data-theme', "light");
-        sessionStorage.theme = "light";
+        document.cookie = "theme=light";
     }
     else {
         document.body.setAttribute('data-theme', "dark");
-        sessionStorage.theme = "dark";
+        document.cookie = "theme=dark";
     }
     // The number of times the theme has been changed is increased by one each time.
     themeToggleCount += 1;
