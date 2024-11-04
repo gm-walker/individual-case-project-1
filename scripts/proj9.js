1737970eea7b63a53f164d67cc4e15c760a42d54
GEE <gm.walker2022@gmail.com>
2024-11-03 21:26:46 -0500

    <ch> Adding Comments for Assignment #9

--- scripts/proj9.js
+++ scripts/proj9.js
@@ -1,6 +1,14 @@
 var themeToggleCount = 0;
 const themeChanger = document.getElementById("theme-chng").children[0];
-
+    /*
+        Code security is critical for the safe use and storage of state 
+        information because it ensures that sensitive information can never be 
+        accessed by those who are unauthorized to view/modify that information. In 
+        my case, I have utilized cookies for a cosmetic purpose only (to store the 
+        state of the 'data-theme' attribute). To further improve my website's 
+        security, I could ensure that cookies are created to store information 
+        related solely to the cosmetics of the website.
+    */
 function readCookie(){
     let cookies = {};
     if(document.cookie){
@@ -33,6 +41,8 @@ function toggleTheme() {
     }
     // The number of times the theme has been changed is increased by one each time.
     themeToggleCount += 1;
+    // Notifies the user that they've changed the theme
+    window.alert(`You've changed the theme!`)
     // Then, the number of theme changes is logged to the console.
     console.log(`Theme has been changed ${themeToggleCount} time(s).`)
 }
