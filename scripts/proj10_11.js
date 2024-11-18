efe37ad8472b14fa5c4959731bd499f420332aed
GEE <gm.walker2022@gmail.com>
2024-11-18 17:58:17 -0500

    <feat> Add initial styling and functionality to Trivia page

---
+++ scripts/proj10_11.js
@@ -0,0 +1,29 @@
+// This file contains the scripts for the Trivia game (CSCN 315 Assignment #10-11)
+
+const trivia_q = [
+    {
+        question: "In what book were the 12 Principles of Animation introduced?",
+        options:{
+            "The Illusion of Life: Disney Animation": true,
+            "The Animator's Survival Kit": false,
+        }
+    },
+    {
+        question: "Who was the father of animation?",
+        options:{
+            "Émile Cohl": true,
+            "Walt Disney": false,
+            "Richard Williams": false,
+        }
+    },
+    {
+        question: "Who worked with Robert Zemeckis to blend live action and animation?",
+        options:{
+            "Richard Williams": true,
+            "Stephen Spielberg": false,
+            "Dale Baer": false,
+        }
+    }
+];
+
+console.log(trivia_q);
