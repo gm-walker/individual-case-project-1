b1349d4e8845f1eb3e2856f6f6a4cf1f6023b781
GEE <gm.walker2022@gmail.com>
2024-11-25 13:34:21 -0500

    <feat> Finish Assignment #10 - Add showFinalScreen - Add score - Add max-width to trivia-options & trivia-field

--- scripts/proj10_11.js
+++ scripts/proj10_11.js
@@ -25,13 +25,93 @@ const trivia_q = [
         }
     }
 ];
+var score = 0;
 
-console.log(trivia_q);
+document.getElementById("trivia-start").onclick = () => {
+    displayQuestion(trivia_q[0]);
+}
+
+function displayNextQuestion(){
+    let question = document.getElementById("t_question");
+    let curIndex = trivia_q.findIndex((q) => q.question.toUpperCase() === question.innerText.toUpperCase())
+    if(curIndex + 1 < trivia_q.length){
+        displayQuestion(trivia_q[curIndex+1]);
+    }
+}
 
 function displayQuestion(question) {
     // Displays the question and its options on the page.
+    let trivia_area = document.getElementById("trivia-game");
+
+    trivia_area.innerHTML = `<h1 id="t_question">${question.question}</h1><div id="trivia-options"></div><div id="trivia-field"></div><button id="trivia-next">Answer</button>`
+    
+    let trivia_options = document.getElementById("trivia-options");
+    let i = 1;
+    for(const keys in question.options){
+        trivia_options.innerHTML += `<div id="q_${i}" class="t_option" draggable="true">${keys}</div>`;
+        i++;
+    }
+    // For all options...
+    document.querySelectorAll(`div[draggable="true"]`).forEach((option)=>{
+        option.addEventListener("dragstart", (event) => {
+            const dt = event.dataTransfer;
+            dt.setData("html", event.target.id);
+            dt.effectAllowed = "copy";
+        })
+    });
+    // When an option is dropped into over the field...
+    document.getElementById("trivia-field").addEventListener("drop", (event)=>{
+        const data = event.dataTransfer.getData("html");
+        const selected = document.getElementById(data);
+        if(event.target.firstChild == null){
+            event.target.appendChild(selected.cloneNode(selected));
+        }
+        else{
+            let old = event.currentTarget.firstChild;
+            event.currentTarget.replaceChild(selected.cloneNode(selected), old);
+        }
+    })
+    // When an option is being dragged over the field...
+    document.getElementById("trivia-field").addEventListener("dragover", (event)=>{
+        event.preventDefault();
+        event.dataTransfer.dropEffect = "copy";
+    })
+
+    // When the answer button is clicked...
+    document.getElementById("trivia-next").onclick = (event)=>{
+        let question = document.getElementById("t_question");
+        let curIndex = trivia_q.findIndex((q) => q.question.toUpperCase() == question.innerText.toUpperCase())
+        let selected = document.getElementById("trivia-field").firstChild.innerText;
+        checkAnswer(trivia_q, curIndex, selected);
+        console.log(curIndex)
+        console.log(trivia_q.length);
+        if(curIndex == (trivia_q.length - 1)){
+            event.currentTarget.innerText = "done";
+            event.currentTarget.onclick = showFinalScreen;
+        }
+        else{
+            event.currentTarget.innerText = "next";
+            event.currentTarget.onclick = displayNextQuestion;
+        }
+
+    }
 }
 
-function checkAnswer(question, response){
+function checkAnswer(trivia, qIndex, response){
     // Checks the answer selected by the user
+    if(trivia[qIndex].options[response]){
+        // If correct...
+        window.alert("This answer is correct.")
+        score++;
+    }
+    else{
+        // Otherwise...
+        window.alert("This answer is incorrect.")
+    }
+}
+
+function showFinalScreen(){
+    const screen = document.getElementById("trivia-game");
+    screen.innerHTML = `<h1>Congrats!</h1><p>You've answered all of the questions.<br>You've answered ${score} question(s) correctly out of ${trivia_q.length}.</p><br><button onclick="displayQuestion(trivia_q[0])">play again</button>`;
+
 }
