375ab5261473e7b12ccf44d29b35a2c9f305580a
GEE <gm.walker2022@gmail.com>
2024-11-30 16:10:27 -0500

    <ch> Add comments for clarification.

--- scripts/proj11.js
+++ scripts/proj11.js
@@ -1,6 +1,9 @@
 const quoteField = document.getElementById("quote");
 
 fetch("https://bible-api.com/?random=verse?translation=kjv")
+    // Parse the retrieved JSON object
     .then(response => response.json())
+    // Inserts HTML containing the verse and the reference in the designated paragraph element
     .then(verse => quoteField.innerHTML = `${verse.text}<br><strong>${verse.reference}</strong>`)
+    // If something goes wrong, alert the client.
     .catch(error => window.alert(error))
